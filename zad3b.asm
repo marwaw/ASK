607f5fcee32f4807193c08608d38579b96d63867
marwaw <martyna.waw96@gmail.com>
2016-05-17 22:30:32 +0200

    Zadania

---
+++ zad3b.asm
@@ -0,0 +1,97 @@
+.data
+	array: .space 100
+	ile: .asciiz "Ile liczb chcesz posortowac? " 
+	podaj: .asciiz "Podaj liczbe: "
+	space: .asciiz ", "
+
+.text
+	.main:		
+		# ile liczb posortowac
+		li $v0, 4
+		la $a0, ile
+		syscall
+			
+		li $v0, 5
+		syscall
+		# $s0 - rozmiar tablicy
+		move $s0, $v0
+		
+		addi $t1, $zero, 0
+		addi $t2, $zero, 0
+			
+		createArr:
+			# wyœwietlenie zapytania
+			li $v0, 4
+			la $a0, podaj
+			syscall
+		
+			# pobranie wartoœci
+			li $v0, 5
+			syscall
+			sw $v0, array($t1)
+		
+			addi $t1, $t1, 4
+			addi $t2, $t2, 1
+			
+			bne $t2, $s0, createArr
+    
+    		jal sort
+   
+        	addi $t0, $zero, 0
+        	addi $t1, $zero, 4
+   		mul $t1, $s0, $t1
+        
+    		print:
+        		beq $t0, $t1, exit
+        		lw $t6, array($t0)
+        		li $v0, 1
+        		add $a0, $t6, $zero
+        		syscall
+        		li $v0, 4
+        		la $a0, space
+        		syscall
+        		addi $t0, $t0, 4
+        		j print
+        
+    		exit:
+        		li $v0, 10
+        		syscall
+
+		sort:
+			addi $sp, $sp, -4
+			sw $ra, 0($sp)
+			# $t0 - rozmiar do posortowania
+   			# i- $t1
+   			# j - $t2
+   			addi $t0, $zero, 4
+   			mul $t0, $s0, $t0 
+   			addi $t1, $zero, 0 
+    	
+   			loop:
+        			beq $t1, $s0, back
+        			addi $t1, $t1, 1
+    				addi $t2, $zero, 0
+    				sub $t0, $t0, 4	
+    				
+        			while:
+	          	 		beq $t2, $t0, loop
+        	    			lw $t3, array($t2)
+	            			addi $t2, $t2, 4
+	           			lw $t4, array($t2)
+	           			slt $t5, $t3, $t4
+	            			bne $t5, $zero, while
+	            			jal swap
+	            			j while
+	            		
+	        	back:
+	        		lw $ra, ($sp)
+	        		addi $sp, $sp, 4
+	        		jr $ra
+	        		
+	        		
+	        	swap:
+	        			sw $t3, array($t2)
+	            			sub $t2, $t2, 4
+	            			sw $t4, array($t2)
+	            			add $t2, $t2, 4
+	            			jr $ra
---
+++ zad4.asm
@@ -0,0 +1,130 @@
+.data
+	wektorPorz1: .space 100
+	wektorWar1: .space 100
+	wektorPorz2: .space 100
+	wektorWar2: .space 100
+	rozmiar: .asciiz "Podaj rozmiar wektorow: "
+	wartosci: .asciiz "Podaj wartosc: "
+	
+	drugi: .asciiz "Podaj drugi wektor \n"
+	wyswietl: .asciiz "Wartosc iloczynu: "
+	
+.text
+
+
+	.main
+		# pytanie o rozmiar wektorow
+		li $v0, 4
+		la $a0, rozmiar
+		syscall
+	
+		# pobranie rozmiaru
+		li $v0, 5
+		syscall
+		move $s0, $v0
+		
+		addi $t0, $zero, 0 # ilosc podanych wartosci
+		addi $t1, $zero, 0 # przesuniecie w wektorze porzadku
+		addi $t2, $zero, 0 # przesuniecie w wektorze wartosci
+	
+		wartosci1:  
+			beq $t0, $s0, dalej
+			addi $t0, $t0, 1
+		
+			li $v0, 4
+			la $a0, wartosci
+			syscall
+		
+			li $v0, 5
+			syscall
+			move $t5, $v0
+			bne $t5, $zero, dodaj
+			
+			# wartosc = 0
+			sw $t5, wektorPorz1($t1)
+			addi $t1, $t1, 4
+			j wartosci1
+		
+			# wartoœæ != 0
+			dodaj:
+				sw $t5, wektorWar1($t2)
+				addi $t2, $t2, 4
+				addi $t3, $zero, 1
+				sw $t3, wektorPorz1($t1)
+				addi $t1, $t1, 4
+				j wartosci1
+	
+		dalej:
+			li $v0, 4
+			la $a0, drugi
+			syscall
+					
+			addi $t0, $zero, 0 # ilosc podanych wartosci
+			addi $t1, $zero, 0 # przesuniecie w wektorze porzadku
+			addi $t2, $zero, 0 # przesuniecie w wektorze wartosci
+	
+		wartosci2:  
+			beq $t0, $s0, iloczynSkalarny
+			addi $t0, $t0, 1
+			li $v0, 4
+			la $a0, wartosci
+			syscall
+		
+			li $v0, 5
+			syscall
+			move $t5, $v0
+			bne $t5, $zero, dodaj2
+		
+			# wartosc = 0
+			sw $t5, wektorPorz2($t1)
+			addi $t1, $t1, 4
+			j wartosci2
+		
+			# wartoœæ != 0
+			dodaj2:
+				sw $t5, wektorWar2($t2)
+				addi $t2, $t2, 4
+				addi $t3, $zero, 1
+				sw $t3, wektorPorz2($t1)
+				addi $t1, $t1, 4
+				j wartosci2
+        
+       	 	iloczynSkalarny:
+        		addi $t0, $zero, 0 # ktora z kolei wartosc
+			addi $t1, $zero, 0 # przesuniecie w wektorze porzadku
+			addi $t2, $zero, 0 # przesuniecie w wektorze wartosci
+			addi $t3, $zero, 0 #aktualna wartosc iloczynu
+		
+			while:
+				beq $t0, $s0, wynik
+				lw $t4, wektorPorz1($t1)
+				beq $t4, $zero, next
+				lw $t5, wektorPorz2($t1)
+				beq $t5, $zero, next
+				lw $t4, wektorWar1($t2)
+				lw $t5, wektorWar2($t2)
+				mul $t4, $t4, $t5
+				add $t3, $t3, $t4
+				addi $t2, $t2, 4
+				
+				addi $t4, $zero, 0
+				addi $t5, $zero, 0
+				
+				next:
+					addi $t1, $t1, 4
+					addi $t0, $t0, 1
+					j while
+		
+        		wynik:
+        			li $v0, 4
+        			la $a0, wyswietl
+        			syscall
+        		
+        			li $v0, 1
+        			add $a0, $t3, $zero
+        			syscall
+        		
+    		exit:
+ 			li $v0, 10
+       			syscall
+			
